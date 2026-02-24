4342c2a23197b7b6ea03533d54ba49e82930c431
noesa <noe.santalucia.7e9@itb.cat>
2026-02-24 20:17:16 +0100

    Portfolio1

--- projects-data.js
+++ projects-data.js
@@ -1,16 +1,5 @@
 window.PROJECTS = [
   {
-    "id": "portfolio-web-1730000000000",
-    "name": "Portfolio personal estilo VS Code",
-    "repoUrl": "https://github.com/tuusuario/tu-repo",
-    "liveUrl": "",
-    "images": [
-      "portfolio1.png",
-      "portfolio2.png"
-    ],
-    "description": "Página personal con sección de currículum y portfolio con cards e imágenes múltiples."
-  },
-  {
     "id": "pagina-lenguaje-marcas-1771958686296",
     "name": "Pagina_Lenguaje_Marcas",
     "repoUrl": "https://github.com/noesantalucia7e9-crypto/DAM01_25_M09.git",
